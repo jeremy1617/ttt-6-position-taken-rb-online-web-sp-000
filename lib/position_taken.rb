5802dd984662443bb9c4838bf1b87919aecfdf49
curious-analyst-4243 <jeremycperry@gmail.com>
2021-02-22 03:47:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " "
     board[index] != " "
-  elsif board[index] == "" 
+  elsif board[index] == ""
     board[index] != ""
   elsif board[index] == nil
     board[index] != nil
